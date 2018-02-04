cee93bf002df0697cf3d9c98f387e2a1e90f0197
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2018-02-03 20:06:45 -0800

    hhdfggff

--- src/app/components/cards/education/education.component.ts
+++ src/app/components/cards/education/education.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonalInfoService } from '../../../service/personal-info.service';
+import { PersonalInfo } from '../../../models/personal-info/personal-info';
 
 @Component({
   selector: 'app-education',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
-
-  constructor() { }
+ 
+  luisejrobles: PersonalInfo;
+  
+  constructor(private personalInfoService: PersonalInfoService) { }
 
   ngOnInit() {
+    this.luisejrobles = this.personalInfoService.personalInfo;
+    console.log(this.luisejrobles);
   }
 
 }
