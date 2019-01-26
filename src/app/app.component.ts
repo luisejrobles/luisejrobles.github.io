431e39d98ab28acd9f1ee383bea61de658eb4dc4
Luis Eduardo Jiménez Robles <luisejimenezrobles@gmail.com>
2019-01-26 01:28:05 -0600

    home component & module created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'luisejrobles-github-io';
+  title = 'luisejrobles.github.io';
 }
