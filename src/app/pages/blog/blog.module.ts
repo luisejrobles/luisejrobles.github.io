7ff2ffb60025eea7b436bfa7a52db6de5dc1f0e4
Luis Eduardo Jiménez Robles <luisejimenezrobles@gmail.com>
2019-01-26 01:34:28 -0600

    blog page component created

---
+++ src/app/pages/blog/blog.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlogComponent } from './blog.component';
+
+@NgModule({
+  declarations: [BlogComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
